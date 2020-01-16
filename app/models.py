85bd7e374cc4c61e9d05fd6075eb3160bebf36ab
Esteban Mejía <estebanmejia277@gmail.com>
2020-01-16 00:11:49 -0500

    Models of sqlite3 created

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from app import app
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50))
+    description = db.Column(db.Text())
+    url = db.Column(db.String(100))
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    message = db.Column(db.Text())
