c648274839144d3027ae6d403225090e32df529b
Esteban Mejía <estebanmejia277@gmail.com>
2020-01-16 00:11:19 -0500

    New file for configurations of the project.

---
+++ app/config.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.abspath(os.getcwd())}/database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
