b9e6524050014cdf5cee01d8d7d53045f324780c
Esteban Mejía <estebanmejia277@gmail.com>
2020-02-29 15:57:13 -0500

    Employment info modified

--- flask/app/forms.py
+++ flask/app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired, Email, ValidationError, Length
 class ContactForm(FlaskForm):
     name = StringField(label='Name', validators=[ DataRequired()])
     email = StringField(label='Email', validators=[ DataRequired(), Email() ])
-    country_code = StringField('Country Code', [DataRequired()], default='+57')
+    country_code = StringField('Code', [DataRequired()], default='+57')
     telephone_number = IntegerField(label = 'Phone number', validators=[ DataRequired()])
     message = TextAreaField(label='Message', validators=[ DataRequired(),
         Length(min=20, message="Message must be at least 20 characters long.") 
